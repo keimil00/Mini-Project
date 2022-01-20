bed9c140125c3abae73fa170b03c20496073aee5
Kamil Nowak <keimil00@gmail.com>
2022-01-20 19:53:19 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "CMax3SatProblem.h"
+
+int main() {
+    CMax3SatProblem testObject;
+    std::string file_address = "C://Users//keimi//CLionProjects//Mini_Project//m3s_50_0.txt";
+    testObject.Load(file_address);
+    testObject.print_clauses();
+    testObject.print_variables();
+    std::vector<bool> test_solution;
+    for (int i = 0; i < 50; ++i) {
+        if(i%2 == 0){
+            test_solution.push_back(true);
+        }else{
+            test_solution.push_back(false);
+        }
+    }
+    std::cout << testObject.Compute(test_solution);
+    return 0;
+}
