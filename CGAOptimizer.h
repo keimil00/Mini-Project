e8f6405eede615fbbfcf8697f3213334df122f1e
Kamil Nowak <keimil00@gmail.com>
2022-01-22 13:11:31 +0100

    git commit -m "Adding implementation for methods Crossover, Mutation and addIndividual."

--- CGAOptimizer.h
+++ CGAOptimizer.h
@@ -15,7 +15,7 @@ public:
     CGAOptimizer(int size_of_population, double probability_of_crossing, double probability_of_mutation);
     void Initialize();
     void RunIteration();
-
+    void addIndividual(CGAIndividual<T>* new_individual);
 private:
     int size_of_population;
     double probability_of_crossing;
@@ -49,16 +49,28 @@ void CGAOptimizer<T>::RunIteration() {
     while(new_population.size() < individuals.size()){
         parent1 = pickParent(individuals);
         parent2 = pickParent(individuals);
-        child1 = parent1->Crossover(parent2);
-        child2 = parent2->Crossover(parent1);
-        child1 = child1->Mutation();
-        child2 = child2->Mutation();
-        addIndividual(new_population, child1, child2);
+        child1 = parent1->Crossover(parent2, probability_of_crossing);
+        child2 = parent2->Crossover(parent1, probability_of_crossing);
+        child1->Mutation(probability_of_mutation);
+        child2->Mutation(probability_of_mutation);
+        addIndividual(child1);
+        addIndividual(child2);
     }
 
 }
 
-
+template<typename T>
+void CGAOptimizer<T>::addIndividual(CGAIndividual<T>* new_individual) {
+    double min = 1;
+    int min_index;
+    for (int i = 0; i < size_of_population; ++i) {
+        if(individuals[i]->Fitness() < min){
+            min = individuals[i]->fitness; //???
+            min_index = i;
+        }
+    }
+    individuals[min_index] = new_individual;
+}
 
 
 #endif //UNTITLED8_CGAOPTIMIZER_H
