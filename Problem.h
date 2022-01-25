ed6bb6416a6b7eb0dcf0369cc307a9b287e327fc
Kamil Nowak <keimil00@gmail.com>
2022-01-25 13:09:54 +0100

    git commit -m "Adding super class Problem for possible future re-use of optimizer."

---
+++ Problem.h
@@ -0,0 +1,19 @@
+//
+// Created by keimi on 24.01.2022.
+//
+
+#ifndef MINI_PROJECT_PROBLEM_H
+#define MINI_PROJECT_PROBLEM_H
+
+#include <iostream>
+#include <vector>
+
+class Problem {
+public:
+    virtual void Load(const std::string& address) = 0;
+    virtual double Compute(std::vector<bool> solution) = 0;
+    virtual int size() = 0;
+};
+
+
+#endif //MINI_PROJECT_PROBLEM_H
